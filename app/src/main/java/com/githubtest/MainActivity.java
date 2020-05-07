9c48111e9ab54e274d8871e8e0c3bc70a4432789
hanshuo <hanshuo@bjhxak.com>
2020-05-07 14:46:01 +0800

    添加onresume

--- app/src/main/java/com/githubtest/MainActivity.java
+++ app/src/main/java/com/githubtest/MainActivity.java
@@ -15,5 +15,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.e(TAG, "onCreate: ");
         Log.e(TAG, "onStart " );
+        Log.e(TAG, "onResume " );
     }
 }
